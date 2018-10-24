51f575d84cbbad288bf18fcb6b5b51d3e2a248e1
AbhishekRaoC <32139755+AbhishekRaoC@users.noreply.github.com>
2018-10-24 12:46:40 +0400

    Update mainrun.py

--- mainrun.py
+++ mainrun.py
@@ -5,4 +5,15 @@ Created on Wed Oct 24 11:27:58 2018
 @author: cabhi
 """
 
+from currency_converter import CurrencyConverter as c
+
+
 print("Oh hello there your early, dont worry it'll be done in a few school days")
+
+reqamt = int(input("What is the amount you want to store in USD: "))
+
+#denom of swiss francs
+chfd = [5, 10, 20, 50, 100, 1000]
+
+#denom of swiss francs in USD value
+chf = [5.01, 10.03, 20.07, 50.17, 100.33, 1003.34 ]
