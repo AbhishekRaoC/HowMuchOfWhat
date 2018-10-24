2216b3a9d2750ce064c9922d88f8dc11af6b1081
AbhishekRaoC <32139755+AbhishekRaoC@users.noreply.github.com>
2018-10-24 11:47:41 +0400

    Update mainrun.py

--- mainrun.py
+++ mainrun.py
@@ -5,4 +5,4 @@ Created on Wed Oct 24 11:27:58 2018
 @author: cabhi
 """
 
-print("Oh hello there your early, dont worry it'll be done in a few days of school")
+print("Oh hello there your early, dont worry it'll be done in a few school days")
